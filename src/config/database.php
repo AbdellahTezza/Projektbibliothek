5ea16e7d1772f83dfe0c14ee3bf6811ac4c42c5c
AbdellahTezza <76401409+AbdellahTezza@users.noreply.github.com>
2025-02-22 00:03:37 +0100

    database

--- src/config/database.php
+++ src/config/database.php
@@ -1,45 +1,19 @@
 <?php
-// class Database {
-    // private $host = "127.0.0.1:4306";
-    // private $db_name = "schulbibliothek";
-    // private $username = "root";
-    // private $password = "";
-    // public $db;
-
-    // public function getConnection() {
-        // $this->db = null;
-        // try {
-            // $this->db = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            // $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // } catch (PDOException $exception) {
-            // echo "Datenbankverbindungsfehler: " . $exception->getMessage();
-        // }
-        // return $this->db;
-    // }
-// }
-?>
-
-
-<?php
 class Database {
     private static $connection = null;
 
-    // Diese Methode stellt eine Verbindung zur Datenbank her
     public static function getConnection() {
         if (self::$connection === null) {
             try {
-                $host = '127.0.0.1:4306'; // Datenbank-Host (z.B. localhost)
-                $db = 'schulbibliothek'; // Name der Datenbank
-                $user = 'root'; // Datenbank-Nutzername
-                $password = ''; // Datenbank-Passwort (Standard für localhost)
+                $host = '127.0.0.1:4306'; 
+                $db = 'schulbibliothek'; 
+                $user = 'root'; 
+                $password = ''; 
                 
-                // DSN (Data Source Name) für PDO
                 $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
                 
-                // PDO-Instanz erstellen und Verbindung herstellen
                 self::$connection = new PDO($dsn, $user, $password);
                 
-                // Fehlerbehandlung aktivieren
                 self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die("Datenbankverbindung fehlgeschlagen: " . $e->getMessage());
